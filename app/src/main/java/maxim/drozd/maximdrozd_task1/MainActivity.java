61509abac6c324e6333e05c1f58cd0ab94d869ad
Max <s312@tut.by>
2019-01-31 14:22:41 +0300

    init commit

---
+++ app/src/main/java/maxim/drozd/maximdrozd_task1/MainActivity.java
@@ -0,0 +1,15 @@
+package maxim.drozd.maximdrozd_task1;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.text.Html;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
