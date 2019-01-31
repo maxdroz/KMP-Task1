2ba127bf446b30cb8d77f08916fb24b42e1d03ff
Max <s312@tut.by>
2019-01-31 22:23:28 +0300

    AppCenter

--- app/src/main/java/maxim/drozd/maximdrozd_task1/MainActivity.java
+++ app/src/main/java/maxim/drozd/maximdrozd_task1/MainActivity.java
@@ -7,6 +7,10 @@ import android.text.method.LinkMovementMethod;
 import android.widget.TextView;
 import com.crashlytics.android.Crashlytics;
 import io.fabric.sdk.android.Fabric;
+import com.microsoft.appcenter.AppCenter;
+import com.microsoft.appcenter.analytics.Analytics;
+import com.microsoft.appcenter.crashes.Crashes;
+import com.microsoft.appcenter.distribute.Distribute;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,6 +19,9 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         Fabric.with(this, new Crashlytics());
         setContentView(R.layout.activity_main);
+
+        AppCenter.start(getApplication(), "537ba06c-9f74-4109-bfa7-18ca47970105",
+                Analytics.class, Crashes.class, Distribute.class);
 
         TextView link = findViewById(R.id.textView2);
         String href = getResources().getString(R.string.Link);
