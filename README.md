88b4c56886d30df4494455eeebe20b7ceb4b9d85
Max <s312@tut.by>
2019-01-31 15:50:43 +0300

    fabric added

---
+++ README.md
@@ -0,0 +1,3 @@
+# KMP-HT1
+
+[![CircleCI](https://circleci.com/gh/Kamerton12/KMP-Task1.svg?style=svg)](https://circleci.com/gh/Kamerton12/KMP-Task1)
--- app/src/main/java/maxim/drozd/maximdrozd_task1/MainActivity.java
+++ app/src/main/java/maxim/drozd/maximdrozd_task1/MainActivity.java
@@ -5,12 +5,15 @@ import android.os.Bundle;
 import android.text.Html;
 import android.text.method.LinkMovementMethod;
 import android.widget.TextView;
+import com.crashlytics.android.Crashlytics;
+import io.fabric.sdk.android.Fabric;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Fabric.with(this, new Crashlytics());
         setContentView(R.layout.activity_main);
 
         TextView link = findViewById(R.id.textView2);
